6b35433f0dfe5870cfa642e80677939160b9a692
BuildTools <unconfigured@null.spigotmc.org>
2018-08-12 23:48:48 +0900

    template codes with handlebars

---
+++ database.js
@@ -0,0 +1,15 @@
+var product = {
+	currency: {
+		name: 'United Staate dollars',
+		abbrev: 'USD',
+	},
+	tours: [
+		{ name: 'Hood River', price: '$99.95'},
+		{ name: 'Oregon Coast', price: '$159.95'},
+	],
+	specialsUrl: '/january-specials',
+	currencies: ['USD', 'GBP', 'BTC'],
+	name: '<b>Buttercup</b>'
+}
+
+exports.product = product;
--- meadowlark.js
+++ meadowlark.js
@@ -3,9 +3,46 @@ var express = require('express'),
 app = express(),
 // 핸들바 뷰 엔진 설정 
 handlebars = require('express-handlebars')
-		.create({ defaultLayout:'main' }),
-fortune = require('./lib/fortune.js');
+		.create({ 
+			defaultLayout:'main',
+			helpers: {
+				section: function(name, options){
+					if(!this._sections) this._sections = {};
+					this._sections[name] = options.fn(this);
+					return null;
+				}
+			}
+}),
+fortune = require('./lib/fortune.js'),
+database = require('./database.js');
 
+function getWeatherData(){
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)',
+			},
+			{
+				name: 'Bend',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)',
+			},
+			{
+				name: 'Manzanita',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light Rain',
+				temp: '55.0 F (12.8 C)',
+			},
+		]
+	}
+}
 
 app
 		.use(express.static(__dirname + '/public'))
@@ -15,6 +52,11 @@ app
 		.use(function(req, res, next){
 			res.locals.showTests = app.get('env') !== 'production' &&
 					req.query.test === '1';
+			next();
+		})
+		.use(function(req, res, next){
+			if(!res.locals.partials) res.locals.partials = {};
+			res.locals.partials.weatherContext = getWeatherData();
 			next();
 		});
 		
@@ -37,12 +79,29 @@ app
 		.get('/tours/request-group-rate', function(req, res){
 			res.render('tours/request-group-rate');
 		})
+		.get('/tours/product-list', function(req, res){
+			res.render('tours/product-list', database.product)
+		})
+		.get('/jquery-test', function(req, res){
+			res.render('jquery-test')
+		})
 		.get('/headers', function(req, res){
 			res.set('Content-Type', 'text/plain');
 			var s = '* client header information*\n\n';
 			for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
 				res.send(s);
-		});
+		})
+		.get('/nursery-rhyme', function(req, res){
+			res.render('nursery-rhyme');
+		})
+		.get('/data/nursery-rhyme', function(req, res){
+			res.json({
+				animal: 'squirrel',
+				bodyPart: 'tail',
+				adjective: 'bushy',
+				noun: 'heck',
+			})
+		})
 
 
 app
@@ -57,6 +116,7 @@ app
 			res.status(500);
 			res.render('500');
 		});
+
 
 app.listen(app.get('port'), function(){
 	console.log('Express started on https://localhost: ' +
